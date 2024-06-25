191d8b049008e497c5c5867a0518b09a2123c137
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2024-06-26 04:23:51 +1000

    Add navigation to app

--- app/src/main/java/com/example/androidtriviastarter/TitleFragment.kt
+++ app/src/main/java/com/example/androidtriviastarter/TitleFragment.kt
@@ -3,9 +3,14 @@ package com.example.androidtriviastarter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
 import com.example.androidtriviastarter.databinding.FragmentTitleBinding
 
@@ -29,6 +34,22 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.options_menu, menu)
+            }
+            override fun onMenuItemSelected(menuItem: android.view.MenuItem): Boolean {
+                // Handle the menu selection
+                when (menuItem.itemId) {
+                    R.id.aboutFragment ->
+                        view?.findNavController()?.navigate(R.id.action_titleFragment_to_aboutFragment)
+                    else -> return false
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
         return binding.root
     }
 }
